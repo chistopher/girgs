4dd49a6387992ade16720cb771f6bce18ef0b6c3
Christopher Weyand <christopher.weyand@student.hpi.de>
2019-01-10 14:28:05 +0100

    tests for AngleHelper

--- source/tests/hypergirgs-test/AngleHelper_test.cpp
+++ source/tests/hypergirgs-test/AngleHelper_test.cpp
@@ -4,6 +4,7 @@
 #include <gmock/gmock.h>
 
 #include <hypergirgs/AngleHelper.h>
+#include <hypergirgs/Hyperbolic.h>
 
 
 using namespace std;
@@ -12,22 +13,58 @@ using namespace hypergirgs;
 
 class AngleHelper_test: public testing::Test
 {
+protected:
+    AngleHelper_test()
+    : n(100)
+    , angles(sampleAngles(n, 12))
+    , radii(sampleRadii(n, 0.75, calculateRadius(n, 0.75, 0, 10), 15))
+    {
+    }
+
+    unsigned int n;
+    vector<double> angles;
+    vector<double> radii;
 };
 
 
-TEST_F(AngleHelper_test, testTreeStructure)
+TEST_F(AngleHelper_test, testCellInsertion)
 {
-    // TODO implement
+    // test that all points are inserted in the correct cells
+    const auto maxLevel = 10u;
+    for (unsigned int level = 0; level < maxLevel; ++level) {
+        for(auto angle : angles) {
+            auto cell = AngleHelper::cellForPoint(angle, level) + AngleHelper::firstCellOfLevel(level);
+            auto bounds = AngleHelper::bounds(cell, level);
+            ASSERT_GE(angle, bounds.first);
+            ASSERT_LT(angle, bounds.second);
+        }
+    }
 }
 
 
 TEST_F(AngleHelper_test, testTouching)
 {
-    // TODO implement
+    auto level = 5u;
+
+    auto a1 = 2.0;
+    auto a2 = 4.0;
+
+    auto cell1 = AngleHelper::cellForPoint(a1, level) + AngleHelper::firstCellOfLevel(level);
+    auto cell2 = AngleHelper::cellForPoint(a2, level) + AngleHelper::firstCellOfLevel(level);
+    ASSERT_FALSE(AngleHelper::touching(cell1, cell2, level));
 }
 
 
 TEST_F(AngleHelper_test, testDistance)
 {
-    // TODO implement
+    auto level = 7u;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            auto cell1 = AngleHelper::cellForPoint(angles[i], level) + AngleHelper::firstCellOfLevel(level);
+            auto cell2 = AngleHelper::cellForPoint(angles[j], level) + AngleHelper::firstCellOfLevel(level);
+            auto actualDist = hyperbolicDistance(radii[i], angles[i], radii[j], angles[j]);
+            auto cellDist = AngleHelper::dist(cell1, cell2, level);
+            ASSERT_LE(cellDist, actualDist);
+        }
+    }
 }
