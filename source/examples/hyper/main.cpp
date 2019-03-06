599ee39fd2cd5d3d1613b752eab3efe09b71a0fc
Christopher Weyand <christopher.weyand@student.hpi.de>
2019-03-06 10:02:12 +0100

    girgs hyper comparison

--- source/examples/hyper/main.cpp
+++ source/examples/hyper/main.cpp
@@ -1,20 +1,57 @@
 
 #include <iostream>
-#include <chrono>
-#include <map>
 #include <string>
 #include <cstring>
-#include <fstream>
+#include <map>
 #include <algorithm>
 
-#include <girgs/girgs-version.h>
+#include <omp.h>
+
 #include <girgs/Generator.h>
 #include <girgs/Hyperbolic.h>
 
+#include <hypergirgs/Hyperbolic.h>
+
 
 using namespace std;
-using namespace chrono;
-using namespace girgs;
+
+using Graph = vector<pair<int,int>>;
+
+
+void createReverseEdges(Graph& g) {
+    const auto n = g.size();
+    g.reserve(2*n);
+    for(int i=0; i<n; ++i)
+        g.push_back({g[i].second, g[i].first});
+}
+
+// returns the difference of two graphs G1, G2
+// G1,2 must be sorted
+// first element contains all edges in G1/G2
+// second element contains all edges in G2/G1
+pair<Graph, Graph> edge_diff(const Graph& g1, Graph& g2) {
+    Graph g1_not_g2;
+    Graph g2_not_g1;
+
+    auto it1 = g1.begin();
+    auto it2 = g2.begin();
+    while(it1!=g1.end() && it2!=g2.end()){
+        if(*it1 == *it2){ // edge is in both lists
+            it1++;
+            it2++;
+        } else if(*it1 < *it2) { // edge only in G1
+            g1_not_g2.push_back(*it1);
+            it1++;
+        } else { // edge only in G2
+            g2_not_g1.push_back(*it2);
+            it2++;
+        }
+    }
+    copy(it1, g1.end(), back_inserter(g1_not_g2));
+    copy(it2, g2.end(), back_inserter(g2_not_g1));
+
+    return {g1_not_g2, g2_not_g1};
+}
 
 
 map<string, string> parseArgs(int argc, char** argv) {
@@ -34,7 +71,7 @@ map<string, string> parseArgs(int argc, char** argv) {
 
 template<typename T>
 void logParam(T value, string name) {
-    cout << "\t" << name << "\t=\t" << value << '\n';
+    clog << "\t" << name << "\t=\t" << value << '\n';
 }
 
 template<typename T>
@@ -50,142 +87,95 @@ void rangeCheck(T value, T min, T max, string name, bool lex = false, bool hex =
 
 
 int main(int argc, char* argv[]) {
-    // TODO rewrite using hypergirgs
     // write help
     if (argc < 2 || 0 == strcmp(argv[1], "--help") || 0 == strcmp(argv[1], "-help")) {
+        clog << "Generates input for a HRG and tries to generate a GIRG with the input mapped as described in paper.\n";
         clog << "usage: ./hyper\n"
              << "\t\t[-n anInt]          // number of nodes                          default 10000\n"
              << "\t\t[-alpha aFloat]     // ple is 2alpha+1          range [0.5,1]   default 0.75\n"
-             << "\t\t[-T aFloat]         // temperature              range [0,1)     default 0\n"
              << "\t\t[-deg aFloat]       // average degree           range [1,n)     default 10\n"
              << "\t\t[-odeg aFloat]      // an offset to desired deg for girg        default 0.0\n"
+             << "\t\t[-step aFloat]      // odeg step during search                  default 0.1\n"
              << "\t\t[-rseed anInt]      // radius seed                              default 12\n"
-             << "\t\t[-aseed anInt]      // angle seed                               default 130\n"
-             << "\t\t[-sseed anInt]      // sampling seed                            default 1400\n"
-             << "\t\t[-file aString]     // file name for output graph               default \"graph\"\n"
-             << "\t\t[-stats 0|1]        // shows difference to actual hyp graph     default 0\n";
+             << "\t\t[-aseed anInt]      // angle seed                               default 130\n";
         return 0;
     }
 
     // read params
     auto params = parseArgs(argc, argv);
     auto n      = !params["n"    ].empty()  ? stoi(params["n"    ]) : 10000;
-    auto alpha  = !params["alpha"].empty()  ? stod(params["alpha"]) : 0.75;
-    auto T      = !params["T"    ].empty()  ? stod(params["T"    ]) : 0;
+    auto hrg_alpha  = !params["alpha"].empty()  ? stod(params["alpha"]) : 0.75;
     auto deg    = !params["deg"  ].empty()  ? stod(params["deg"  ]) : 10.0;
     auto odeg   = !params["odeg"].empty()   ? stod(params["odeg" ]) : 0.0;
+    auto step   = !params["step"].empty()   ? stod(params["step" ]) : 0.1;
     auto rseed  = !params["rseed"].empty()  ? stoi(params["rseed"]) : 12;
     auto aseed  = !params["aseed"].empty()  ? stoi(params["aseed"]) : 130;
-    auto sseed  = !params["sseed"].empty()  ? stoi(params["sseed"]) : 1400;
-    auto file   = !params["file" ].empty()  ? params["file"] : "graph";
-    auto stats  = params["stats"] == "1";
 
     // log params and range checks
-    cout << "using:\n";
+    clog << "using:\n";
     logParam(n, "n");
-    rangeCheck(alpha, 0.5, 1.0, "alpha");
-    rangeCheck(T, 0.0, 1.0, "T", false, true);
+    rangeCheck(hrg_alpha, 0.5, 1.0, "alpha");
     rangeCheck(deg, 1.0, n-1.0, "deg");
     logParam(odeg, "odeg");
+    logParam(step, "step");
     logParam(rseed, "rseed");
     logParam(aseed, "aseed");
-    logParam(sseed, "sseed");
-    logParam(file, "file");
-    logParam(stats, "stats");
-    auto R = calculateRadius(n, alpha, T, deg);
-    logParam(R, "R");
-    cout << endl;
+    clog << endl;
 
+    omp_set_num_threads(1);
+    auto T = 0.0; // compare two models in threshold versions
+    auto sseed = 0; // sampling seed is irrelevant in threshold model
 
-    cout << "sampling radii ...\t\t" << flush;
-    auto t1 = high_resolution_clock::now();
-    auto radii = sampleRadii(n, alpha, R, rseed);
-    auto t2 = high_resolution_clock::now();
-    cout << "done in " << duration_cast<milliseconds>(t2 - t1).count() << "ms"<< endl;
+    clog << "generate HRG input and edges ";
+    auto R = hypergirgs::calculateRadius(n, hrg_alpha, T, deg);
+    auto radii = hypergirgs::sampleRadii(n, hrg_alpha, R, rseed, false);
+    auto angles = hypergirgs::sampleAngles(n, aseed, false);
+    auto hrg_edges = hypergirgs::generateEdges(radii, angles, T, R, sseed);
+    clog << "(deg = " << hrg_edges.size()*2.0 / n << ")\n";
+    clog << "generated HRG edges: " << hrg_edges.size() << '\n';
+    createReverseEdges(hrg_edges);
+    sort(hrg_edges.begin(), hrg_edges.end());
 
-    cout << "sampling angles ...\t\t" << flush;
-    auto angles = sampleAngles(n, aseed);
-    auto t3 = high_resolution_clock::now();
-    cout << "done in " << duration_cast<milliseconds>(t3 - t2).count() << "ms" << endl;
-
-    cout << "converting to girg ...\t\t" << flush;
-    auto girg_alpha = T == 0 ? std::numeric_limits<double>::infinity() : 1.0 / T;
+    clog << "converting input\n";
+    auto girg_alpha = std::numeric_limits<double>::infinity();
     auto girg_weights = vector<double>(n, 1.0);
     auto girg_positions = vector<vector<double>>(n, vector<double>(1));
     for(int i = 0; i < n; ++i) {
-        girg_weights[i] = radiusToGirgWeight(radii[i], R);
-        girg_positions[i][0] = angleToGirgPosition(angles[i]);
-    }
-    auto t4 = high_resolution_clock::now();
-    cout << "done in " << duration_cast<milliseconds>(t4 - t3).count() << "ms" << endl;
-
-
-    cout << "sampling edges for girg ...\t" << flush;
-    scaleWeights(girg_weights, deg+odeg, 1, girg_alpha);
-    auto edges = generateEdges(girg_weights, girg_positions, girg_alpha, sseed);
-    auto t5 = high_resolution_clock::now();
-    cout << "done in " << duration_cast<milliseconds>(t5 - t4).count() << "ms\tavg deg = " << 2.0*edges.size() / n << endl;
-
-
-    // compute some stats
-    if(!stats)
-        return 0;
-
-    cout << "computing stats ...\t\t" << flush;
-    auto t6 = high_resolution_clock::now();
-
-    // init accumulators
-    long long missing_edges = 0;
-    long long false_edges = 0;
-    auto missing_deviation = 0.0;
-    auto false_deviation = 0.0;
-
-    // normalize adj matrix that edges point from smaller index to larger
-    auto adj_list = vector<vector<int>>(n);
-    for(auto& edge : edges) {
-            auto mm = minmax(edge.first, edge.second);
-            adj_list[mm.first].push_back(mm.second);
+        girg_weights[i] = girgs::radiusToGirgWeight(radii[i], R);
+        girg_positions[i][0] = girgs::angleToGirgPosition(angles[i]);
     }
 
-    // check all point pairs
-    auto current_neighs = std::vector<char>(n, false);
-    for(int i=0; i<n; ++i){
-        // get one row in adj matrix
-        std::fill(current_neighs.begin(), current_neighs.end(), false);
-        for(auto neigh : adj_list[i])
-            current_neighs[neigh] = true;
-        // check that all edges of i are correct in hyperbolic space
-        for(int j=i+1; j<n; ++j){
-            auto edge_present = current_neighs[j];
-            auto dist = girgs::hyperbolicDistance(radii[i], angles[i], radii[j], angles[j]);
-            if(dist < R) {
-                if(!edge_present) {
-                    missing_deviation += R - dist;
-                    missing_edges++;
-                }
-            } else {
-                if(edge_present) {
-                    false_deviation += dist - R;
-                    false_edges++;
-                }
-            }
-        }
-    }
+    auto diff = [&](double desired_degree) {
+        clog << "generate GIRG edges ";
+        auto scaled_weights = girg_weights;
+        auto scaling = girgs::scaleWeights(scaled_weights, desired_degree, 1, girg_alpha);
+        auto girg_edges = girgs::generateEdges(scaled_weights, girg_positions, girg_alpha, sseed);
+        clog << "\t(scaling = " << scaling << " deg = " << girg_edges.size()*2.0 / n << ")\n";
+        createReverseEdges(girg_edges);
+        sort(girg_edges.begin(), girg_edges.end());
+        auto sub = edge_diff(hrg_edges, girg_edges);
+        clog << "HRG/GIRG " << sub.first.size()/2 << "\tGIRG/HRG " << sub.second.size()/2 << '\n';
+        cout << desired_degree << ',' << sub.first.size()/2 << ',' << sub.second.size()/2 << ',' << scaling << ',' << girg_edges.size()/2 << '\n';
+        return sub;
+    };
 
-    auto t7 = high_resolution_clock::now();
-    auto total_edges = edges.size();
-    cout << "done in " << duration_cast<milliseconds>(t7 - t6).count() << "ms" << endl;
-    cout << '\n';
-    cout << "total edges       " << total_edges << '\n';
-    cout << '\n';
-    cout << "missing edges     " << missing_edges << '\n';
-    cout << "missing deviation " << missing_deviation << '\n';
-    cout << "average mistake   " << missing_deviation / missing_edges << '\n';
-    cout << '\n';
-    cout << "false edges       " << false_edges << '\n';
-    cout << "false deviation   " << false_deviation << '\n';
-    cout << "average mistake   " << false_deviation / false_edges << '\n';
-    cout << '\n';
-    cout << "(false+missing)/total " << static_cast<double>(false_edges+missing_edges)/total_edges << '\n';
+    auto high = odeg;
+    auto low = odeg-step;
+
+    cout << "deg,hrg_only,girg_only,scaling,girg_edges\n";
+    clog << "scale girg desired avg up until girg is a super-graph of HRG\n";
+    do {
+        auto sub = diff(deg+high);
+        if(sub.first.empty()) break;
+        high += step;
+    } while(true);
+
+    clog << "scale girg desired avg down until girg is a sub-graph of HRG\n";
+    do {
+        auto sub = diff(deg+low);
+        if(sub.second.empty()) break;
+        low -= step;
+    } while(true);
+
     return 0;
 }
