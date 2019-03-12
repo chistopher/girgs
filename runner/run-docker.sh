122f8dfcf5487f9f8f7dd9fa76336dd190342e0a
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2019-03-12 21:21:29 +0100

    Use hypergirg benchmark in container

--- runner/run-docker.sh
+++ runner/run-docker.sh
@@ -4,6 +4,6 @@ mkdir -p $DATADIR
 
 DOCKER_CMD="docker run --rm -it -u $(id -u ${USER}):$(id -g ${USER})"
 
+$DOCKER_CMD -v $DATADIR:/data -t girgs_hypergirgs
 $DOCKER_CMD -v $DATADIR:/data -t girgs_networkit
 $DOCKER_CMD -v $DATADIR:/data -t girgs_girgs
-$DOCKER_CMD -v $DATADIR:/data -t girgs_hypergirgs
