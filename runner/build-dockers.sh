cef17a5fcf0e77235f0f73624fa0e3feb5702d63
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2019-03-12 19:54:18 +0100

    Add docker images for all generators (some not working yet)

--- runner/build-dockers.sh
+++ runner/build-dockers.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+# the common image installs C++, Python and tools used by all other containers
+docker build -t girgs_common common
+
 docker build -t girgs_networkit networkit
+
+cd girgs; git clone ../.. source
+docker build -t girgs_girgs
+
+cd hypergirgs; git clone ../.. source
+docker build -t girgs_hypergirgs hypergirgs
+
 docker build -t girgs_hypergen  hypergen
-docker build -t girgs_girgs     girgs
--- runner/run-docker.sh
+++ runner/run-docker.sh
@@ -5,3 +5,5 @@ mkdir -p $DATADIR
 DOCKER_CMD="docker run --rm -it -u $(id -u ${USER}):$(id -g ${USER})"
 
 $DOCKER_CMD -v $DATADIR:/data -t girgs_networkit
+$DOCKER_CMD -v $DATADIR:/data -t girgs_girgs
+$DOCKER_CMD -v $DATADIR:/data -t girgs_hypergirgs
