3cfe3620b1c5a4568f791ad36ca0db67ea2991d2
Thomas Bläsius <thomas.blaesius@kit.edu>
2019-04-17 17:20:20 +0200

    runner script for running comparison experiments sequentially

---
+++ runner/run-docker-seq.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+DATADIR=$(pwd)/data-seq
+mkdir -p $DATADIR
+
+DOCKER_CMD="docker run --rm -it -u $(id -u ${USER}):$(id -g ${USER}) --hostname $(hostname) -e OMP_NUM_THREADS=1"
+
+$DOCKER_CMD -v $DATADIR:/data girgs_embedder
+$DOCKER_CMD -v $DATADIR:/data --entrypoint /usr/bin/benchmark-nkopt.sh -t girgs_hypergen
+$DOCKER_CMD -v $DATADIR:/data --entrypoint /usr/bin/benchmark-hypergen.sh -t girgs_hypergen
+# $DOCKER_CMD -v $DATADIR:/data -t girgs_hypergen
+$DOCKER_CMD -v $DATADIR:/data -t girgs_hypergirgs
+$DOCKER_CMD -v $DATADIR:/data -t girgs_networkit
+# $DOCKER_CMD -v $DATADIR:/data -t girgs_girgs
